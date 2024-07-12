5bf2d09e8a25ab9a905133cfa71b1d5ce292b24e
khaidprogramming <khalidmiya399@gmail.com>
2024-07-12 19:14:47 +0545

    fixed css at postcard

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -103,7 +103,7 @@ const PostCard = ({ d ,fetchPosts}) => {
     
    
     <div  className={` dark:bg-black  mr-64 sm:mr-24 flex ${darkMode ? 'dark-mode' : 'light-mode'} items-center justify-center`}>
-    <div className={`bg-white dark:bg-gray-800  text-black dark:border-gray-800 p-4 ${darkMode ? 'dark-mode' : 'light-mode'} rounded-xl border max-w-xl`}>
+    <div className={`bg-white dark:bg-gray-800  text-black dark:border-gray-800 p-4 ${darkMode ? 'dark-mode' : 'light-mode'} rounded-xl border min-w-72  max-w-72 sm:max-w-xl`}>
       <div className="flex justify-between">
         <div className="flex items-center">
           <img className="h-11 w-11 rounded-full" src={profileImageUrl} alt="Profile"/>
@@ -130,7 +130,7 @@ const PostCard = ({ d ,fetchPosts}) => {
             <g><path d="M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z"></path></g>
           </svg>
           {/* <div className="ml-3 px-32">post</div> */}
-         { isMyProfile && <div onClick={()=>handleDelete(_id)} className=" ml-64 text-lg cursor-pointer"><AiOutlineDelete/></div>}
+         { isMyProfile && <div onClick={()=>handleDelete(_id)} className=" ml-24 sm:ml-64 text-lg cursor-pointer"><AiOutlineDelete/></div>}
          { isSuperAdmin && <div onClick={()=>handleDelete(_id)} className=" ml-64 text-lg cursor-pointer"><AiOutlineDelete/></div>}
 
         </div>
